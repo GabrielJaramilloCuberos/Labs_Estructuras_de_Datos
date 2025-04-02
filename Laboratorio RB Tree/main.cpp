668a1a232eb765ba768b6af5b5a588d4de1d0283
GabrielJaramilloCuberos <124003921+GabrielJaramilloCuberos@users.noreply.github.com>
2025-04-02 12:02:20 -0500

    Update main.cpp

--- Laboratorio RB Tree/main.cpp
+++ Laboratorio RB Tree/main.cpp
@@ -8,7 +8,10 @@
 #include "arbolrb.h"
 
 int main() {
+    // Creación de un Árbol Rojo-Negro
     RBTree tree;
+    
+    // Inserción de nodos en el árbol
     tree.insert(11);
     tree.insert(19);
     tree.insert(7);
@@ -18,11 +21,15 @@ int main() {
     tree.insert(26);
     tree.insert(45);
     tree.insert(70);
+    
+    // Mostrar el recorrido en inorden (izquierda, raíz, derecha)
     cout << "Inorder Traversal of Created Tree\n";
     tree.inorder();
+    
+    // Mostrar el recorrido por niveles (nivel por nivel)
     cout << "\n\nLevel Order Traversal of Created Tree\n";
     tree.levelOrder();
+    
     cout << "\n";
     return 0;
 }
-
